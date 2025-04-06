828488f5b7f9d92abca3c08b674e74812c47f3b4
ShadowProgrammist <serzh.frolov.2004@inbox.ru>
2025-04-06 22:32:07 +0300

    modified dev-Main v.3

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке dev");
         System.out.println("Изменение 1");
+        System.out.println("Изменение 2");
     }
 }
